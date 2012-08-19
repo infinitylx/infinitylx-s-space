84a8527c774741eb85a99c8aba501103ae3f76cd
Wladislaw Merezhko <infinitylx@infinitylx.org.ua>
2012-08-19 19:47:46 +0300

    Setup settings for my blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,29 +4,31 @@
 AUTHOR = u"Wladislaw Merezhko"
 SITENAME = u"Infinity's space!"
 SITEURL = 'http://blog.infinitylx.org.ua'
-PDF_GENERATOR = False
-FEED_ATOM= 'feeds/all.atom.xml'
+PDF_GENERATOR = False # FIXME: found why this is doesn work
+FEED_ATOM = 'feeds/all.atom.xml'
 FEED_RSS = 'feeds/all.rss.xml'
 TIMEZONE = 'Europe/Kiev'
 
 DEFAULT_LANG = 'en'
 
 ARTICLE_DIR = 'blog'
-DEFAULT_METADATA = (('yeah', 'it is'),)
+DEFAULT_METADATA = (('yeah', 'it is'),)  # ???
 DIPLAY_PAGES_ON_MENU = True
 THEME = "theme"
 # Blogroll
-LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+# for now no links... my be later add some usefull links
+# LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+#           ('Python.org', 'http://python.org'),
+#           ('Jinja2', 'http://jinja.pocoo.org'),
+#           ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('FB', '#'),
-          ('Twit me', '#'),)
+# SOCIAL = (('FB', '#'),
+#           ('Twit me', '#'),)
 
 DEFAULT_PAGINATION = 10
-STATIC_PATHS = ["pictures", ]
+STATIC_PATHS = ["pictures", "images"]
 DEFAULT_CATEGORY = 'blog'
 TYPOGRIFY = True
 RELATIVE_URLS = True  # False - only for prod mode
+TWITTER_USERNAME = "infinitylX"
