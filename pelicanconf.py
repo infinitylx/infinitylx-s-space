0c1f5e87a469084dbb846dfa6a2a0f466a2835c1
Wladislaw Merezhko <infinitylx@infinitylx.org.ua>
2013-01-30 21:26:40 +0200

    Porting to 3.2 version

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,13 +3,15 @@
 
 AUTHOR = u"Wladislaw Merezhko"
 SITENAME = u"Infinity's space!"
-SITEURL = 'http://infinitylx.org.ua'
+SITEURL = 'http://infinitylx.org'
 PDF_GENERATOR = False  # FIXME: fix links workaround in rst2pdf
 # PDF_STYLE = 'test_greek'  # Name of style wich rst2pdf will use
 # PDF_STYLE_PATH = '/home/infinitylx/tmp/infinitylx-s-space/'
 FEED_ATOM = 'feeds/all.atom.xml'
 FEED_RSS = 'feeds/all.rss.xml'
 TIMEZONE = 'Europe/Kiev'
+
+PLUGINS = [ 'pelican.plugins.sitemap', 'pelican.plugins.gzip_cache' ]
 
 DEFAULT_LANG = 'en'
 
@@ -32,11 +34,11 @@ DEFAULT_PAGINATION = 10
 STATIC_PATHS = ["pictures", "images"]
 DEFAULT_CATEGORY = 'blog'
 TYPOGRIFY = True
-RELATIVE_URLS = False  # False - only for prod mode
+RELATIVE_URLS = True  # False - only for prod mode
 TWITTER_USERNAME = "infinitylX"
 
 GOOGLE_ANALYTICS = "UA-34537389-1"
-YANDEX_METRYKA = True
+YANDEX_METRYKA = "16922653"
 
 GITHUB_URL = 'https://github.com/infinitylx'
 GITHUB_POSITION = 'right'
@@ -44,3 +46,9 @@ GITHUB_POSITION = 'right'
 DISQUS_SITENAME = "infinitylxsspace"
 
 SHORT_MAIL = 'infinitylX@shortmail.com'
+
+SITEMAP = {
+    'format': 'xml', 
+    'priorities': {'articles': 0.8, 'pages': 0.6, 'indexes': 1}, 
+    'changefreqs': {'articles': 'monthly', 'pages': 'monthly', 'indexes': 'weekly'}
+}
